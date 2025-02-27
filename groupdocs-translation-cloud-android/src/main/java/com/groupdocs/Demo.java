4ecbb50cafbdebe7a5135e89ea5fe2f8bdc16925
Alena Panyugina <alena.panyugina@aspose.com>
2025-02-27 10:35:11 +0300

    Android 25.2

--- groupdocs-translation-cloud-android/src/main/java/com/groupdocs/Demo.java
+++ groupdocs-translation-cloud-android/src/main/java/com/groupdocs/Demo.java
@@ -11,20 +11,35 @@ public class Demo {
         String clientSecret = "YOUR_CLIENT_SECRET";
 
         ApiClient defaultClient = new ApiClient(basePath, cliendId, clientSecret, null);
-        TranslationApi apiInstance = new TranslationApi(defaultClient);
-
+        TranslationApi translationApi = new TranslationApi(defaultClient);
 
         TextRequest request = new TextRequest();
-        request.setSourceLanguage("en");
-        request.addTargetLanguagesItem("de");
-        request.addTextsItem("Text to translate");
 
+        request.setSourceLanguage("en");
+        request.addTargetLanguagesItem("nl");
+        request.addTextsItem("Text to translate");
+        request.setOrigin("");
 
         try {
-            String r = apiInstance.textPost(request).getId();
-            CloudTextResponse cloudTextResponse = apiInstance.textRequestIdGet(r);
-            System.out.println(cloudTextResponse);
-        } catch (ApiException e) {
+            String r = translationApi.textPost(request).getId();
+            CloudTextResponse response = translationApi.textRequestIdGet(r);
+            if (!response.getStatus().toString().equals("500")) {
+                while (true) {
+                    response = translationApi.textRequestIdGet(r);
+                    if (response.getStatus().toString().equals("200")) {
+                        System.out.println(response);
+                        break;
+                    }
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        }
+        catch(ApiException e){
             System.err.println("Exception when calling TranslationApi#textPost");
             System.err.println("Status code: " + e.getCode());
             System.err.println("Reason: " + e.getResponseBody());
@@ -33,5 +48,13 @@ public class Demo {
         }
     }
 }
+
+
+
+
+
+
+
+
 
 
